78e7162feaa46b4489dba37cf307b1b44690b63f
Evan Torkos <etorkos@gmail.com>
2015-03-02 16:36:08 -0500

    stated on routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,5 +1,37 @@
 'use strict';
 var router = require('express').Router();
+var User = require('../../db/models/user.js');
+var Item = require('../../db/models/item.js');
 module.exports = router;
 
 router.use('/tutorial', require('./tutorial'));
+
+function isAuthenticated (req, res, next){
+	if(req.user) next();
+	else{
+		var err = new Error('Unknown user');
+		err.status =401;
+		next(err);
+	}
+}
+
+
+router.get('/user', function(req, res, err){
+	var email = req.data.email;  //should be structured to include username: username
+	User.findOne({username}).exec(function(err, user){
+		if (err) return next(err);
+		res.send(user);
+	})
+})
+
+router.get('/itemlist', function(req, res, err){  //should be requested by angular when page loads
+	Item.find({}).exec(function(err, users){
+		if(err) return next(err);
+		res.send(users);
+	})
+})
+
+router.get('/item:id', function(req, res, err){ //requested by angular when item is selected
+	var itemId = req.params.id;
+	Item.find({id: item})
+})
