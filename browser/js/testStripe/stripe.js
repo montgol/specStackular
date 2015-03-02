4387bff39ca28f043cd99e0ff9805108235d204f
blakeprobinson <bprobinson@zoho.com>
2015-03-02 18:36:45 -0500

    stripe test added

---
+++ browser/js/testStripe/stripe.js
@@ -0,0 +1,15 @@
+'use strict';
+app.config(function ($stateProvider) {
+
+    // Register our *about* state.
+    $stateProvider.state('stripe', {
+        url: '/stripe',
+        controller: 'StripeController',
+        templateUrl: 'js/testStripe/stripe.html'
+    });
+
+});
+
+app.controller('StripeController', function ($scope) {
+
+});
