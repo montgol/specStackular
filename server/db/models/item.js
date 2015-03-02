6ff24136e701b826a3ce34d26277d046b53fc4df
Evan Torkos <etorkos@gmail.com>
2015-03-02 18:10:44 -0500

    items seem correct, needs to be tested

--- server/db/models/item.js
+++ server/db/models/item.js
@@ -12,8 +12,19 @@ var item = new mongoose.Schema({
     availability: boolean,
     imgUrl: String,
     categories: [String],
-    reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}]
+    reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}],
+    options: [{type: Schema.Types.ObjectId, ref: 'Options'}]
 })
+
+item.static.getReviews = function(){  //need to make sure syntax is correct
+	this.reviews.populate('Review', function(err, reviews){
+		if (err) return err;
+		console.log(reviews) // for testing purposes only
+		return reviews;
+	})
+
+
+}
 
 var review = new mongoose.Schema({
 	userId: {type: Number, required: true, ref: 'item'},
@@ -33,14 +44,8 @@ var option = new mongoose.Schema({
 	priceModifier: {type: Number, required: true}
 })
 
-item.static.getReviews = function(){  //need to make sure syntax is correct
-	this.reviews.populate('Review', function(err, reviews){
-		if (err) return err;
-		console.log(reviews) // for testing purposes only
-		else return reviews;
-	})
-}
+var Item = mongoose.model('Item', item);
+var Review = mongoose.model("Review", review);
+var Feature = mongoose.model("Option", option);
 
-
-
-mongoose.model = ('Item', item);
+module.exports = {Item: Item, Review: Review, Feature: Feature};
