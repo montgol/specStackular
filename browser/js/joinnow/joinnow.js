e2866f76dff787047b26fa3540a4fc4fc0c9dc44
Glenn Davis <glenn@gdcreative.com>
2015-03-03 15:25:03 -0500

    Added container class to index.html, created joinnow and create-account forms

---
+++ browser/js/joinnow/joinnow.js
@@ -0,0 +1,10 @@
+app.config(function ($stateProvider) {
+
+    // Register our *Join Now* state.
+    $stateProvider.state('join', {
+        url: '/join',
+        //controller: 'joinController',
+        templateUrl: 'js/joinnow/joinnow.html'
+    });
+
+});
