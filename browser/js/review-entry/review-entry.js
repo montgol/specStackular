3743d50203c43d5eba903b5e64f6b67b0572b52f
Glenn Davis <glenn@gdcreative.com>
2015-03-05 15:07:07 -0500

    Handoff to Lynda for help with review controller

--- browser/js/review-entry/review-entry.js
+++ browser/js/review-entry/review-entry.js
@@ -3,12 +3,10 @@ app.config(function ($stateProvider) {
     // Register our *Review Entry* state.
     $stateProvider.state('review-entry', {
         url: ':item/review-entry',
-        controller: 'reviewEntryController',
-        templateUrl: 'js/review-entry/review-entry.html'
+        controller: function($scope, $state, $stateParams) {
+            console.log($stateParams);},
+        templateUrl: 'js/review-entry/review-entry.html',
     });
 
 });
 
-app.controller('reviewEntryController', function($scope, $state, $stateParams) {
-    console.log($stateParams.item.name);
-});
