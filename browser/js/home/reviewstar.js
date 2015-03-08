04261a5c3d5ca03750ff0cadf4a11de067bbec59
Glenn Davis <glenn@gdcreative.com>
2015-03-08 10:25:53 -0400

    Stopping working on fake home page due to bizarro angular/css problems. Next up: Make this the product page, make the real fake home page, and do the dropdown menus.

---
+++ browser/js/home/reviewstar.js
@@ -0,0 +1,50 @@
+app
+
+    .constant('ratingConfig', {
+        max: 5,
+    })
+
+    .directive('reviewstar', ['ratingConfig', function(ratingConfig) {
+        return {
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                value: '=',
+            },
+            template: '<div id="showme" ng-mouseleave="reset()"><i id="showme" ng-repeat="number in range" ng-mouseenter="enter(number)" ng-click="assign(number)" ng-class="{\'glyphicon glyphicon-star icon-gold\': number <= val, \'glyphicon glyphicon-star icon-gray\': number > val}"></i></div>',
+            link: function(scope, element, attrs) {
+                var maxRange = angular.isDefined(attrs.max) ? scope.$eval(attrs.max) : ratingConfig.max;
+
+                scope.range = [];
+                for(var i = 1; i <= maxRange; i++ ) {
+                    scope.range.push(i);
+                }
+
+                scope.$watch('value', function(value) {
+                    scope.val = value;
+                });
+
+                scope.assign = function(value) {
+                    scope.value = value;
+                }
+
+                scope.enter = function(value) {
+                    scope.val = value;
+                }
+
+                scope.reset = function() {
+                    scope.val = angular.copy(scope.value);
+                }
+                scope.reset();
+
+            }
+        };
+    }]);
+
+app.controller('StarCtrl', function($scope) {
+
+    $scope.rate1 = 0;
+
+    $scope.rate2 = 6;
+
+});
