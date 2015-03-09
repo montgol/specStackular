e2f3dc88c831243ff64f9e81c6989ab7a7e73866
Glenn Davis <glenn@gdcreative.com>
2015-03-09 10:45:29 -0400

    Got the review stars thing on the product page to work

--- browser/js/home/reviewstar.js
+++ browser/js/home/reviewstar.js
@@ -9,27 +9,35 @@ app
             restrict: 'EA',
             replace: true,
             scope: {
-                value: '=',
+                value: '='
             },
-            template: '<div id="showme" ng-mouseleave="reset()"><i id="showme" ng-repeat="number in range" ng-mouseenter="enter(number)" ng-click="assign(number)" ng-class="{\'glyphicon glyphicon-star icon-gold\': number <= val, \'glyphicon glyphicon-star icon-gray\': number > val}"></i></div>',
-            link: function(scope, element, attrs, ) {
+            controller: "productreviewscontroller",
+            template: '<div id="showme" ng-mouseleave="reset()"><i id="showme" ng-repeat="number in range" ' +
+                'ng-mouseenter="enter(number)" ng-click="assign(number)" ' +
+                'ng-class="{\'glyphicon glyphicon-star icon-gold\': number <= val, ' +
+                '\'glyphicon glyphicon-star icon-gray\': number > val}"></i></div>',
+            link: function(scope, element, attrs, index) {
                 var maxRange = angular.isDefined(attrs.max) ? scope.$eval(attrs.max) : ratingConfig.max;
-
                 scope.range = [];
                 for(var i = 1; i <= maxRange; i++ ) {
                     scope.range.push(i);
-                }
+                };
 
-                scope.val =
+                scope.val = scope.value;
+
+                console.log(scope);
+
 
             }
         };
     }]);
 
-app.controller('StarCtrl', function($scope) {
+app.controller('productstar', function($scope) {
 
     $scope.rate1 = 0;
 
     $scope.rate2 = 6;
+
+    $scope.val = $scope.rating;
 
 });
